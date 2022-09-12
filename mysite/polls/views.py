302786156f073a94cd6a301ef8e1327b619015b4
wazzzarakhudu <info@tablemate.net>
2022-09-12 01:11:39 -0400

    Basic testing tutorials complete

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from .models import Question,Choice
 from  django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 from django.http import Http404
 from django.template import loader
 # Create your views here.
@@ -13,12 +14,18 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        :return:
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
